3902affde97f70351a3598cafefe63acd5215f6c
Taylor J. Dawson <taylorjdawson@gmail.com>
2017-07-30 17:06:53 -0700

    Added feedback button with associated non-functional modal.

--- js/main.js
+++ js/main.js
@@ -24,6 +24,14 @@
 
     console.log('The DOM may not be ready');
 
+    function isEmpty(obj) {
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key))
+                return false;
+        }
+        return true;
+    }
+
     function search(text) {
         var dynamicData = {};
         dynamicData["search"] = text;
@@ -35,6 +43,7 @@
         });
     }
 
+    //TODO: Change name
     function testDataattr() {
         var min = $(this).data()['time']['minutes'];
         var sec = $(this).data()['time']['seconds'];
@@ -47,10 +56,14 @@
             $("#yes_btn").data("url",url);
         });
 
-
+        //TODO: Change name
         $('#myModal').modal('show')
 
         //alert('Line id' + $(this).data());
+    }
+
+    function launch_feedback_modal() {
+        $('#feedbackModal').modal('show')
     }
 
     $("#yes_btn").click(function () {
@@ -67,6 +80,8 @@
 
     $("#search_button").click(function() {
 
+
+
         var search_box = $('#search_text');
         var searchText = search_box.val();
 
@@ -77,50 +92,65 @@
         search(searchText)
 
             .success(function(data) {
-                //TODO: CHECK if data is empty    
-                var list = $("#quotes_list");
-                var i;
-                for(i = 0; i < data.length; i++) {
 
-                    var li = document.createElement('li');
-                    li.className = 'list-group-item';
-                    li.id = data[i]["id"];
-                    li.innerHTML = '<blockquote>' + data[i]['line'] + '</blockquote>';
+                if(!isEmpty(data)){
+
+                    $('#no_results').hide();
+
+                    var list = $("#quotes_list");
+                    var i;
+                    for(i = 0; i < data.length; i++) {
+
+                        var li = document.createElement('li');
+                        li.className = 'list-group-item';
+                        li.id = data[i]["id"];
+                        li.innerHTML = '<blockquote>' + data[i]['line'] + '</blockquote>';
 
 
-                    var btn = document.createElement('button');
-                    btn.typeName = 'button';
-                    btn.className = 'btn btn-link icon-button';
-                    btn.style = "outline:none;";
-                    btn.dataset.time = JSON.stringify(data[i]['time']);
-                    btn.dataset.ep_id = data[i]['id'].substring(0,4);
+                        var btn_view = document.createElement('button');
+                        btn_view.typeName = 'button';
+                        btn_view.className = 'btn btn-link icon-button';
+                        btn_view.style = "outline:none;";
+                        btn_view.dataset.time = JSON.stringify(data[i]['time']);
+                        btn_view.dataset.ep_id = data[i]['id'].substring(0,4);
 
-                    var spnIcon = document.createElement('span');
-                    spnIcon.title = 'View';
-                    spnIcon.className = "glyphicon glyphicon-eye-open";
-                    //spnIcon.hidden = 'true';
+                        var btn_feedback = btn_view.cloneNode(true);
+                        btn_feedback.className = 'btn btn-link icon-button pull-right';
 
-                    btn.appendChild(spnIcon);
+                        var spn_icon_view = document.createElement('span');
+                        spn_icon_view.title = 'View';
+                        spn_icon_view.className = "glyphicon glyphicon-eye-open";
 
-                    var spn = document.createElement('span');
+                        var spn_icon_feedback = document.createElement('span');
+                        spn_icon_feedback.title = 'Feedback';
+                        spn_icon_feedback.className = "glyphicon glyphicon-info-sign";
 
-                    spn.appendChild(btn);
+                        btn_view.appendChild(spn_icon_view);
+                        btn_feedback.appendChild(spn_icon_feedback);
 
-                    li.appendChild(spn);
-                    btn.onclick = testDataattr;
+                        var spn = document.createElement('span');
 
-                    // dynamicItems += '<li class="list-group-item" data-time="'+ data[i]['time'] +'" id="'+ data[i]["id"]+'"><blockquote>' + data[i]['line'] + '</blockquote>Season: ' +
-                    //     data[i]['season']['number'] + ' Episode: ' + data[i]['episode']['number'] + ' Time: ' + data[i]['time']['minutes'] + 'm ' + data[i]['time']['seconds']+'s' +
-                    //     '<br><span><button type="button" class="btn btn-link icon-button" style="outline:none;" >' +
-                    //     '<span title="View" aria-hidden="true" class="glyphicon glyphicon-eye-open"></span></button></span></li>'
+                        // spn.appendChild(btn_view);
 
-                    list[0].appendChild(li);
+                        li.appendChild(btn_view);
+                        li.appendChild(btn_feedback);
+
+                        btn_view.onclick = testDataattr;
+                        btn_feedback.onclick = launch_feedback_modal;
+
+                    // <button type="button" class="btn btn-link icon-button pull-right" style="outline:none;" >
+                    //         <span title="Feedback" aria-hidden="true" class="glyphicon glyphicon-info-sign"></span>
+                    //         </button>
+
+                        list[0].appendChild(li);
+                    }
+                } else {
+                    var div = document.createElement('div');
+                    div.innerHTML = 'No results for: "' + searchText + '"';
+                    div.className = "text-center";
+                    $('#quotes_list')[0].appendChild(div);
+                    console.log('no results');
                 }
-
-
-
-                //list.html(dynamicItems);
-
             })
 
             .fail(function(jqXhr) {
