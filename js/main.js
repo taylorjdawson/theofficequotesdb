5a48c2f0a572bcc72b663cd406910430707f0e3c
Taylor J. Dawson <taylorjdawson@gmail.com>
2017-07-31 18:03:25 -0700

    Added a reporting feature for users to submit feedback on individual quotes.

--- js/main.js
+++ js/main.js
@@ -24,6 +24,8 @@
 
     console.log('The DOM may not be ready');
 
+
+    //Test whether or not a JSON object is empty
     function isEmpty(obj) {
         for(var key in obj) {
             if(obj.hasOwnProperty(key))
@@ -32,6 +34,7 @@
         return true;
     }
 
+    //Issues an http request to search server
     function search(text) {
         var dynamicData = {};
         dynamicData["search"] = text;
@@ -43,8 +46,32 @@
         });
     }
 
-    //TODO: Change name
-    function testDataattr() {
+    function createGitIssue(issue_type, issue_text, line_id, line) {
+        var dynamicData = {};
+
+        dynamicData["issue_type"] = encodeURIComponent(issue_type);
+        dynamicData["issue_text"] = encodeURIComponent(issue_text);
+        dynamicData["line_id"] = encodeURIComponent(line_id);
+        dynamicData["line"] = encodeURIComponent(line);
+        $.ajax({
+            url: "http://localhost:9220/",
+            type: "post",
+            data: dynamicData,
+            contentType: "application/json"
+        })
+            .success(function () {
+                $('#feedbackModal').find('.modal-header .modal-title').html("Result");
+                $('#feedbackModal').find('.modal-body').html("Your report has been submitted.")
+                $('#feedback-form').remove();
+            })
+
+            //TODO: Add modal error message
+            .error(function(jqXhr) {
+            alert('There was a problem sending report. Status: ' + jqXhr.status);
+        });
+    }
+
+    function viewQuoteVideo() {
         var min = $(this).data()['time']['minutes'];
         var sec = $(this).data()['time']['seconds'];
         var ep_id = $(this).data()['ep_id'];
@@ -53,23 +80,48 @@
             var url = json_data[ep_id] + '?t=' + min + 'm' + sec + 's';
             console.log(json_data[ep_id] + '?t=' + min + 'm' + sec + 's');
 
-            $("#yes_btn").data("url",url);
+            $("#leaveSiteModalYesBtn").data("url",url);
         });
 
-        //TODO: Change name
-        $('#myModal').modal('show')
-
-        //alert('Line id' + $(this).data());
+        $('#leaveSiteModal').modal('show')
     }
 
-    function launch_feedback_modal() {
+    function launchFeedbackModal() {
+        var id = $(this).data()['id'];
+        var line = $('#'+id).find('blockquote:first-child').text();
+
+        //Attach the data to the feedback modal's report button
+        $("#feedbackModalReportBtn").data({"id": id, "line": line});
         $('#feedbackModal').modal('show')
     }
 
-    $("#yes_btn").click(function () {
-        $('#myModal').modal('hide');
+    $("#leaveSiteModalYesBtn").click(function () {
+        $('#leaveSiteModal').modal('hide');
        var url = $(this).data("url").toString();
         window.open(url);
+    });
+
+    //TODO: Check for empty form
+    //TODO: Since it is a form maybe change it to 'submit' (possibly seek out internet justification)
+    $('#feedbackModalReportBtn').click(function () {
+        var issue_type = $('#feedback-modal-selector').find('option:selected').text();
+        var message_text = $('#feedback-modal-message-text').val();
+        var line_id = $(this).data('id').toString();
+
+        //TODO: Fix line it is coming back as undefined
+        var line = $(this).data('line').toString();
+
+        createGitIssue(issue_type, message_text, line_id, line);
+
+        console.log(line);
+
+        $('#feedback-form')[0].reset();
+        $('#feedback-modal-selector').selectpicker('refresh');
+    });
+    //TODO: reuse more code (best practices)
+    $('#feedbackModalCancelBtn').click(function () {
+        $('#feedback-form')[0].reset();
+        $('#feedback-modal-selector').selectpicker('refresh');
     });
 
     $("#search_text").keyup(function(event) {
@@ -77,6 +129,8 @@
             $("#search_button").click();
         }
     });
+
+
 
     $("#search_button").click(function() {
 
@@ -101,18 +155,21 @@
                     var i;
                     for(i = 0; i < data.length; i++) {
 
+                        //Create new list element and set attributes
                         var li = document.createElement('li');
                         li.className = 'list-group-item';
                         li.id = data[i]["id"];
                         li.innerHTML = '<blockquote>' + data[i]['line'] + '</blockquote>';
 
-
+                        //Create the view icon button and set attributes
                         var btn_view = document.createElement('button');
                         btn_view.typeName = 'button';
                         btn_view.className = 'btn btn-link icon-button';
                         btn_view.style = "outline:none;";
                         btn_view.dataset.time = JSON.stringify(data[i]['time']);
+                        btn_view.dataset.id = data[i]['id'];
                         btn_view.dataset.ep_id = data[i]['id'].substring(0,4);
+
 
                         var btn_feedback = btn_view.cloneNode(true);
                         btn_feedback.className = 'btn btn-link icon-button pull-right';
@@ -128,19 +185,11 @@
                         btn_view.appendChild(spn_icon_view);
                         btn_feedback.appendChild(spn_icon_feedback);
 
-                        var spn = document.createElement('span');
-
-                        // spn.appendChild(btn_view);
-
                         li.appendChild(btn_view);
                         li.appendChild(btn_feedback);
 
-                        btn_view.onclick = testDataattr;
-                        btn_feedback.onclick = launch_feedback_modal;
-
-                    // <button type="button" class="btn btn-link icon-button pull-right" style="outline:none;" >
-                    //         <span title="Feedback" aria-hidden="true" class="glyphicon glyphicon-info-sign"></span>
-                    //         </button>
+                        btn_view.onclick = viewQuoteVideo;
+                        btn_feedback.onclick = launchFeedbackModal;
 
                         list[0].appendChild(li);
                     }
@@ -156,20 +205,6 @@
             .fail(function(jqXhr) {
                 alert('There was a problem with the request. Status: ' + jqXhr.status);
             });
-
-        // // Dynamically building an unordered list from an array
-        // var localArr = ["Greg", "Peter", "Kyle", "Danny", "Mark"],
-        //     list = $("#quotes_list"),
-        //     dynamicItems = "";
-        //
-        // $.each(localArr, function(index, value) {
-        //
-        //     dynamicItems += "<li id=" + index + ">" + value + "</li>";
-        //
-        // });
-        //
-        // list.append(dynamicItems);
-
     });
 
 }));
